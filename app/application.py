e2aa3368caf670b4451cfb8016c684bd6445df8a
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-09-26 18:27:36 +0200

    Text for translation

--- app/application.py
+++ app/application.py
@@ -24,7 +24,6 @@ import os
 import re
 
 from babel import localedata
-from flask_babel import gettext as __
 from flask_babel import lazy_gettext as _
 import flask
 import flask_babel
@@ -109,7 +108,7 @@ def robots():
 def test():
   return flask.render_template(
     'test.html',
-    title='Test',
+    title=_('Test'),
     html_class='test',
   )
 
@@ -128,7 +127,7 @@ def verify():
   return flask.render_template(
     'account/verify_email.html',
     html_class='account verify',
-    title='Verify Account',
+    title=_('Verify Account'),
     status='error' if util.param('success') is None else 'success',
     url=url,
     key=key,
@@ -144,7 +143,7 @@ def verify_phone(code):
   return flask.render_template(
     'account/verify_phone.html',
     html_class='account phone',
-    title='Verify Phone',
+    title=_('Verify Phone'),
     url='%s/%s' % (config.REDIRECT_PHONE_URL, code),
   )
 
@@ -161,7 +160,7 @@ def forgot():
     email = (util.param('email') or '').lower().strip()
 
     if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-      error = 'That does not look like an email.'
+      error = _('That does not look like an email.')
       status = 'error'
     else:
       try:
@@ -175,22 +174,22 @@ def forgot():
         if result.status_code == 201:
           return flask.redirect(flask.url_for('forgot', success=True))
         elif result.status_code == 400:
-          error = 'This is email is not in use.'
+          error = _('This is email is not in use.')
           status = 'error'
         elif result.status_code == 409:
-          error = 'We already sent you an email. The link is valid for 10 minutes.'
+          error = _('We already sent you an email. The link is valid for 10 minutes.')
           status = 'error'
         else:
-          error = 'Something went wrong, please try again.'
+          error = _('Something went wrong, please try again.')
           status = 'error'
       except requests.exceptions.ConnectTimeout:
-        error = 'That took longer than usual, please try again.'
+        error = _('That took longer than usual, please try again.')
         status = 'error'
 
   return flask.render_template(
     'account/forgot.html',
     html_class='account forgot',
-    title='Change Password',
+    title=_('Change Password'),
     status=status if util.param('success') is None else 'success',
     error=error,
   )
@@ -215,7 +214,7 @@ def reset():
     password = util.param('password')
 
     if len(password) < 8:
-      error = 'Choose a password that is at least 8 characters.'
+      error = _('Choose a password that is at least 8 characters.')
       status = 'fail'
     elif key and code:
       try:
@@ -230,16 +229,16 @@ def reset():
         if result.status_code == 400:
           status = 'error'
         else:
-          error = 'Something went wrong, please try again.'
+          error = _('Something went wrong, please try again.')
           status = 'fail'
       except requests.exceptions.ConnectTimeout:
-        error = 'That took longer than usual, please try again.'
+        error = _('That took longer than usual, please try again.')
         status = 'fail'
 
   return flask.render_template(
     'account/reset.html',
     html_class='account reset',
-    title='Password reset',
+    title=_('Password reset'),
     status=status if util.param('success') is None else 'success',
     error=error,
     code=code,
@@ -291,7 +290,7 @@ def delete():
   return flask.render_template(
     'account/delete.html',
     html_class='account delete',
-    title='Delete Account',
+    title=_('Delete Account'),
     status=status if util.param('success') is None else 'success',
     key=key,
     code=code,
