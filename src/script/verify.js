89e55e82de7fae747c621f0e051e2e97b2aa071c
Gregor Herdmann <gregor@wire.com>
2017-10-11 17:53:15 +0200

    feat: Remove redirect to settings for team accounts (#20)

--- src/script/verify.js
+++ src/script/verify.js
@@ -40,40 +40,6 @@ window.initVerify = function() {
   }
 };
 
-window.checkForAccess = function() {
-  var backendUrl = $('#url').data('backend-url');
-  var teamsUrl = $('#url').data('redirect-teams');
-  if (backendUrl && teamsUrl) {
-    $.ajax({
-      url: backendUrl + '/access',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      xhrFields: {withCredentials: true}
-    }).done(function(data, status_text, xhr) {
-      redirectToTeams(backendUrl, teamsUrl, data);
-    });
-  }
-}
-
-window.redirectToTeams = function (backendUrl, teamsUrl, accessTokenData) {
-  $.ajax({
-    url: backendUrl + '/teams',
-    headers: {
-      'Authorization': accessTokenData.token_type + ' ' + accessTokenData.access_token,
-      'Content-Type': 'application/json',
-    }
-  }).done(function(data, status_text, xhr) {
-    var teams = data.teams.filter(function(team){
-      return team.binding === true;
-    });
-    if (teams[0]) {
-      window.location.href = teamsUrl;
-    }
-  });
-}
-
 window.verifyFail = function(status) {
   $('.loading').hide();
   if (status === 404) {
@@ -86,9 +52,9 @@ window.verifyFail = function(status) {
 window.verifySuccess = function(status) {
   $('.loading').hide();
   $('.' + status).removeClass('hide');
-  checkForAccess();
-  var redirectApp = $('#url').data('redirect');
-  if (redirectApp) {
-    window.location.href = redirectApp;
+  var redirect = $('#url').data('redirect');
+  if (redirect) {
+    var redirectApp = $('#url').data('redirect');
+    window.location.href = redirect;
   }
 };
