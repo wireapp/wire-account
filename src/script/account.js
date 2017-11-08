6ad71ff03507a518cdb7b94c70d9cac944b07a85
Lipis <lipiridis@gmail.com>
2017-11-08 12:50:35 +0200

    Add Travis support and more (#25)

--- src/script/account.js
+++ src/script/account.js
@@ -1,7 +1,7 @@
 window.initForms = function() {
-  $('form').submit(function(event){
+  $('form').submit(function(event) {
     $button = $(this).find('button:submit');
     $button.prop('disabled', true);
     $button.text($button.data('loading-text'));
   });
-}
+};
