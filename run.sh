200a2787866b95b7c79556df03f16eaeb84978c6
Michael Koppen <michael.koppen@googlemail.com>
2018-11-14 15:11:21 +0100

    fix: Correct index.js path

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # override that environment variable) which is why we handle it specifically here
 #
  port=${NODE_PORT:-8080}
- PORT=$port node /src/dist/index.js
+ PORT=$port node /deploy/dist/index.js
