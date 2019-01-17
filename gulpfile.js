ef537c9db49b106f828dd31ae16c39e762f5d59e
Benny Neugebauer <bn@bennyn.de>
2019-01-17 12:37:44 +0100

    overhaul

--- gulpfile.js
+++ gulpfile.js
@@ -34,17 +34,58 @@ const config = {
 const onError = function() {
   $.util.beep();
   return this.emit('end');
-};
+}.bind(this);
 
 const timestamp = function() {
   return moment(new Date()).format('YYYY-MM-DD-hh-mm-ss');
 };
 
-gulp.task('init', 'Initial Tasks.', $.sequence('clean', 'ext:dev', 'script:dev', 'style:dev'));
+gulp.task('clean', function() {
+  del('dist/static/script/');
+  return del('dist/static/style/');
+});
 
-gulp.task('default', 'Watch for changes and reload browser automatically.', $.sequence('init', ['watch', 'reload']));
+gulp.task('ext:dev', function() {
+  return gulp
+    .src(config.ext)
+    .pipe($.plumber({errorHandler: onError}))
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('ext.js'))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist/static/script'));
+});
 
-gulp.task('style', false, function() {
+gulp.task('script:dev', function() {
+  return gulp
+    .src(config.script)
+    .pipe($.plumber({errorHandler: onError}))
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('script.js'))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist/static/script'));
+});
+
+gulp.task('style:dev', function() {
+  return gulp
+    .src(config.style)
+    .pipe($.plumber({errorHandler: onError}))
+    .pipe($.sourcemaps.init())
+    .pipe($.less())
+    .pipe($.autoprefixer({map: true}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('dist/static/style'));
+});
+
+gulp.task(
+  'init',
+  gulp.series('clean', 'ext:dev', 'script:dev', 'style:dev', function(done) {
+    done();
+  }),
+);
+
+gulp.task('default', $.sequence('init', ['watch', 'reload']));
+
+gulp.task('style', function() {
   return gulp
     .src(config.style)
     .pipe($.plumber({errorHandler: onError}))
@@ -59,18 +100,7 @@ gulp.task('style', false, function() {
     .pipe(gulp.dest('dist/static/style'));
 });
 
-gulp.task('style:dev', false, function() {
-  return gulp
-    .src(config.style)
-    .pipe($.plumber({errorHandler: onError}))
-    .pipe($.sourcemaps.init())
-    .pipe($.less())
-    .pipe($.autoprefixer({map: true}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/static/style'));
-});
-
-gulp.task('ext', false, function() {
+gulp.task('ext', function() {
   return gulp
     .src(config.ext)
     .pipe($.plumber({errorHandler: onError}))
@@ -80,17 +110,7 @@ gulp.task('ext', false, function() {
     .pipe(gulp.dest('dist/static/script'));
 });
 
-gulp.task('ext:dev', false, function() {
-  return gulp
-    .src(config.ext)
-    .pipe($.plumber({errorHandler: onError}))
-    .pipe($.sourcemaps.init())
-    .pipe($.concat('ext.js'))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/static/script'));
-});
-
-gulp.task('script', false, function() {
+gulp.task('script', function() {
   return gulp
     .src(config.script)
     .pipe($.plumber({errorHandler: onError}))
@@ -100,43 +120,28 @@ gulp.task('script', false, function() {
     .pipe(gulp.dest('dist/static/script'));
 });
 
-gulp.task('script:dev', false, function() {
-  return gulp
-    .src(config.script)
-    .pipe($.plumber({errorHandler: onError}))
-    .pipe($.sourcemaps.init())
-    .pipe($.concat('script.js'))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/static/script'));
-});
-
-gulp.task('reload', false, function() {
+gulp.task('reload', function() {
   $.livereload.listen(35729);
   return gulp.watch(['dist/static/**/*.{css,js}', 'dist/templates/**/*.{html}']).on('change', $.livereload.changed);
 });
 
-gulp.task('watch', false, function() {
+gulp.task('watch', function() {
   gulp.watch('src/style/**/*.less', ['style:dev']);
   gulp.watch(config.ext, ['ext:dev']);
   return gulp.watch(config.script, ['script:dev']);
 });
 
-gulp.task('clean', false, function() {
-  del('dist/static/script/');
-  return del('dist/static/style/');
-});
-
-gulp.task('version', false, function() {
+gulp.task('version', function() {
   return fs.writeFileSync('dist/version', timestamp());
 });
 
-gulp.task('zip', 'Zip Stuff.', function() {
+gulp.task('zip', function() {
   return gulp
     .src(['package.json', 'dist/**/*', '!dist/*.zip'], {dot: true, nodir: true})
     .pipe($.zip('wire-account.zip'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', 'Build to prepare for deployment.', $.sequence('clean', 'ext', 'script', 'style', 'version', 'zip'));
+gulp.task('build', $.sequence('clean', 'ext', 'script', 'style', 'version', 'zip'));
 
-gulp.task('dist', 'Creates Elastic Beanstalk ZIP file for production uploads.', $.sequence('clean', 'build'));
+gulp.task('dist', $.sequence('clean', 'build'));
