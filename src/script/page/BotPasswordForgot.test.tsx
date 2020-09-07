605722400a63a66e3b33640602b46ff7dc0acdb2
Michael Koppen <michael.koppen@googlemail.com>
2020-09-07 13:49:53 +0200

    test: Add Jest test setup

---
+++ src/script/page/BotPasswordForgot.test.tsx
@@ -0,0 +1,55 @@
+/*
+ * Wire
+ * Copyright (C) 2020 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import '../util/test/mock/matchMediaMock';
+
+import BotPasswordForgot from './BotPasswordForgot';
+import TestPage from '../util/test/TestPage';
+
+jest.mock('script/util/SVGProvider', () => {
+  return {logo: undefined};
+});
+
+class BotPasswordForgotPage extends TestPage<{}> {
+  constructor(props?: {}) {
+    super(BotPasswordForgot, props);
+  }
+
+  getEmailInput = () => this.get('input[data-uie-name="enter-email"]');
+  getResetPasswordButton = () => this.get('button[data-uie-name="do-send-password-reset-email"]');
+  getError = () => this.get('Text[data-uie-name="error-message"]');
+
+  enterEmail = (email: string) => this.changeValue(this.getEmailInput(), email);
+
+  submitResetEmail = () => this.submit(this.getResetPasswordButton());
+}
+
+describe('BotPasswordForgot', () => {
+  it('shows error on invalid email', async () => {
+    const invalidEmail = 'aaa';
+    const botPasswordForgot = new BotPasswordForgotPage();
+
+    expect(botPasswordForgot.getEmailInput().exists()).toBe(true);
+    botPasswordForgot.enterEmail(invalidEmail);
+    botPasswordForgot.submitResetEmail();
+
+    expect(botPasswordForgot.getError().exists()).toBe(true);
+    expect(botPasswordForgot.getError().text()).toEqual('That does not look like an email.');
+  });
+});
---
+++ src/script/util/test/TestUtil.tsx
@@ -0,0 +1,48 @@
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {mount} from 'enzyme';
+import React from 'react';
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import {StyledApp} from '@wireapp/react-ui-kit';
+import {createMemoryHistory, History} from 'history';
+import {Router} from 'react-router';
+const enUS = require('i18n/en-US.json');
+
+export const withRouter = (component: React.ReactElement, history: History) => (
+  <Router history={history}>{component}</Router>
+);
+
+export const withTheme = (component: React.ReactElement): React.ReactElement => <StyledApp>{component}</StyledApp>;
+
+export const withTranslations = (component: React.ReactElement): React.ReactElement => {
+  i18n.use(initReactI18next).init({
+    fallbackLng: 'en-US',
+    lng: 'en-US',
+    preload: ['en-US'],
+    resources: {
+      ['en-US']: enUS,
+    },
+  });
+  return component;
+};
+
+export const mountComponent = (component: React.ReactElement, history: History = createMemoryHistory()) =>
+  mount(withTranslations(withTheme(withRouter(component, history))));
