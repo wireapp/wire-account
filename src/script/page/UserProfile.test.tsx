9bf91e8b931c48e71225e1c9063090b84360358b
Michael Koppen <michael.koppen@googlemail.com>
2022-01-13 17:31:12 +0100

    feat: User profile landing page

---
+++ src/script/page/UserProfile.test.tsx
@@ -0,0 +1,112 @@
+/*
+ * Wire
+ * Copyright (C) 2021 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import '../util/test/mock/matchMediaMock';
+
+import React from 'react';
+import * as History from 'history';
+import {UserProfileProps, UserProfile} from './UserProfile';
+import TestPage from '../util/test/TestPage';
+import {ActionProvider, actionRoot} from '../module/action';
+import {RecursivePartial} from '@wireapp/commons/src/main/util/TypeUtil';
+import {Runtime} from '@wireapp/commons';
+
+jest.mock('script/util/SVGProvider', () => {
+  return {logo: undefined};
+});
+
+class UserProfilePage extends TestPage<UserProfileProps> {
+  constructor(props?: UserProfileProps, root?: RecursivePartial<typeof actionRoot>) {
+    super(
+      () => (
+        <ActionProvider contextData={root as typeof actionRoot}>
+          <UserProfile {...props} />
+        </ActionProvider>
+      ),
+      props,
+    );
+  }
+
+  getOpenApp = () => this.get('a[data-uie-name="open-user-profile-app"]');
+  getOpenWebapp = () => this.get('a[data-uie-name="open-user-profile-webapp"]');
+  getDirectDownload = () => this.get('a[data-uie-name="go-direct-download"]');
+  getWebsiteDownload = () => this.get('a[data-uie-name="go-website-download"]');
+}
+
+describe('UserProfile', () => {
+  describe('on mobile', () => {
+    it('shows open app & direct download', async () => {
+      spyOn(Runtime, 'isMobileOS').and.returnValue(true);
+
+      const conversationJoinPage = new UserProfilePage(
+        {
+          history: undefined,
+          location: History.createLocation('/user-profile'),
+          match: undefined,
+        },
+        {},
+      );
+
+      expect(conversationJoinPage.getOpenApp().exists()).toBe(true);
+      expect(conversationJoinPage.getOpenWebapp().exists()).toBe(false);
+      expect(conversationJoinPage.getOpenApp().exists()).toBe(true);
+      expect(conversationJoinPage.getDirectDownload().exists()).toBe(true);
+    });
+  });
+
+  describe('on desktop', () => {
+    it('shows open app, webapp & direct download on MacOS', async () => {
+      spyOn(Runtime, 'isMobileOS').and.returnValue(false);
+      spyOn(Runtime, 'isMacOS').and.returnValue(true);
+
+      const conversationJoinPage = new UserProfilePage(
+        {
+          history: undefined,
+          location: History.createLocation('/user-profile'),
+          match: undefined,
+        },
+        {},
+      );
+
+      expect(conversationJoinPage.getOpenApp().exists()).toBe(true);
+      expect(conversationJoinPage.getOpenWebapp().exists()).toBe(true);
+      expect(conversationJoinPage.getDirectDownload().exists()).toBe(true);
+      expect(conversationJoinPage.getWebsiteDownload().exists()).toBe(false);
+    });
+
+    it('shows open app, webapp & website download on non-MacOS', async () => {
+      spyOn(Runtime, 'isMobileOS').and.returnValue(false);
+      spyOn(Runtime, 'isMacOS').and.returnValue(false);
+
+      const conversationJoinPage = new UserProfilePage(
+        {
+          history: undefined,
+          location: History.createLocation('/user-profile'),
+          match: undefined,
+        },
+        {},
+      );
+
+      expect(conversationJoinPage.getOpenApp().exists()).toBe(true);
+      expect(conversationJoinPage.getOpenWebapp().exists()).toBe(true);
+      expect(conversationJoinPage.getDirectDownload().exists()).toBe(false);
+      expect(conversationJoinPage.getWebsiteDownload().exists()).toBe(true);
+    });
+  });
+});
---
+++ src/script/page/UserProfile.tsx
@@ -0,0 +1,79 @@
+/*
+ * Wire
+ * Copyright (C) 2021 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+import {Runtime} from '@wireapp/commons';
+import {pathWithParams} from '@wireapp/commons/src/main/util/UrlUtil';
+import {ButtonLink, ContainerSM, FlexBox, H2, QUERY, QueryKeys, Text, useMatchMedia} from '@wireapp/react-ui-kit';
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {DirectDownloadButton} from 'script/component/DirectDownloadButton';
+import Document from 'script/component/Document';
+import {WebsiteDownloadButton} from 'script/component/WebsiteDownloadButton';
+import {WEBAPP_URL, BRAND_NAME, REDIRECT_USER_PROFILE_URL} from 'script/Environment';
+
+export interface UserProfileProps extends React.HTMLProps<Document>, RouteComponentProps<{}> {}
+
+const USER_ID_KEY = 'id';
+
+export const UserProfile = ({location}: UserProfileProps) => {
+  const [t] = useTranslation('userProfile');
+  const isMobile = useMatchMedia(QUERY[QueryKeys.TABLET_DOWN]);
+
+  const params = new URLSearchParams(location.search);
+  const userId = params.get(USER_ID_KEY);
+
+  return (
+    <Document>
+      <ContainerSM css={{margin: 'auto 0'}}>
+        <>
+          <H2 style={{fontWeight: 500, marginBottom: 40, marginTop: '0'}}>{t('title', {brandName: BRAND_NAME})}</H2>
+          <Text block>{t('description')}</Text>
+          <FlexBox column={isMobile} css={{marginTop: 24}}>
+            {Runtime.isMobileOS() || Runtime.isMacOS() ? (
+              <DirectDownloadButton style={{justifyContent: 'center', marginRight: 16}}>
+                {t('downloadApp', {brandName: BRAND_NAME})}
+              </DirectDownloadButton>
+            ) : (
+              <WebsiteDownloadButton style={{justifyContent: 'center', marginRight: 16}} />
+            )}
+
+            <ButtonLink
+              href={pathWithParams(`${REDIRECT_USER_PROFILE_URL}${userId}`)}
+              style={{marginRight: 16}}
+              data-uie-name="open-user-profile-app"
+            >
+              {t('openWithApp')}
+            </ButtonLink>
+
+            {!Runtime.isMobileOS() && (
+              <ButtonLink
+                href={pathWithParams(`${WEBAPP_URL}/#/user/${userId}`)}
+                data-uie-name="open-user-profile-webapp"
+              >
+                {t('openWithBrowser')}
+              </ButtonLink>
+            )}
+          </FlexBox>
+        </>
+      </ContainerSM>
+    </Document>
+  );
+};
+
+export default withRouter(UserProfile);
